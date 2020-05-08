37cc6fc0617e8745f255c924e3597502a9abee1a
Miguel Rios <miguel@adianta.com.br>
2020-05-08 02:08:16 -0300

    :tada: passing tests!

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,24 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    const { income, outcome } = transactions.reduce(
+      (balance, { type, value }) => ({
+        ...balance,
+        [type]: balance[type] + Number(value),
+      }),
+      {
+        income: 0,
+        outcome: 0,
+      },
+    );
+
+    return {
+      income,
+      outcome,
+      total: income - outcome,
+    };
   }
 }
 
